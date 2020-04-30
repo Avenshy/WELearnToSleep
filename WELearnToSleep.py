9bfd5eda0955f8f423773af33f310011194dca9b
MxEmerson <2382413024@qq.com>
2020-04-30 14:59:01 +0800

    fix bugs

--- WELearnToSleep.py
+++ WELearnToSleep.py
@@ -29,6 +29,7 @@ req = session.get(url)
 uid = req.text[req.text.find('"uid":') + 6:req.text.find('"',req.text.find('"uid":') + 7) - 2]
 classid = req.text[req.text.find('classid=') + 8:req.text.find('&',req.text.find('classid=') + 9)]
 i = 0
+way1su, way2su, way1fa, way2fa = 0, 0, 0, 0
 data = '{"cmi":{"completion_status":"completed","interactions":[],"launch_data":"","progress_measure":"1","score":{"scaled":"100","raw":"100"},"session_time":"0","success_status":"unknown","total_time":"0","mode":"normal"},"adl":{"data":[]},"cci":{"data":[],"service":{"dictionary":{"headword":"","short_cuts":""},"new_words":[],"notes":[],"writing_marking":[],"record":{"files":[]},"play":{"offline_media_id":"9999"}},"retry_count":"0","submit_time":""}}[INTERACTIONINFO]'
 url = 'https://welearn.sflep.com/ajax/StudyStat.aspx?action=scoLeaves&cid=' + cid + '&uid=' + uid + '&unitidx=' + str(i) + '&classid=' + classid
 req = session.get(url,headers={'Referer':'https://welearn.sflep.com/2019/student/course_info.aspx?cid=' + cid})
@@ -39,20 +40,30 @@ while '异常' not in req.text and '出错了' not in req.text:
         if('未' in x['iscomplete']):
             print(x['location'] + '...',end='')
             url = 'https://welearn.sflep.com/Ajax/SCO.aspx'
-            req = session.post(url,data={'action':'setscoinfo','cid':cid,'scoid':id,'uid':uid,'data':data,'isend':'False'},headers={'Referer':'https://welearn.sflep.com/Student/StudyCourse.aspx'})
+            req = session.post(url,data={'action':'startsco160928','cid':cid,'scoid':id,'uid':uid,'nocache':'0.0429450926459094'},headers={'Referer':'https://welearn.sflep.com/Student/StudyCourse.aspx?cid={}&classid={}&sco={}'.format(cid, classid, id)})
+            req = session.post(url,data={'action':'setscoinfo','cid':cid,'scoid':id,'uid':uid,'data':data,'isend':'False'},headers={'Referer':'https://welearn.sflep.com/Student/StudyCourse.aspx?cid={}&classid={}&sco={}'.format(cid, classid, id)})
             if('"ret":0' in req.text):
                 print('Way1:Success!!!',end='')
+                way1su += 1
             else:
                 print('Way1:Fail!!!',end='')
+                way1fa += 1
             url = 'https://welearn.sflep.com/Ajax/SCO.aspx'
-            req = session.post(url,data={'action':'savescoinfo160928','cid':cid,'scoid':id,'uid':uid,'progress':'100','crate':'100','status':'unknown','cstatus':'completed','trycount':'0'},headers={'Referer':'https://welearn.sflep.com/Student/StudyCourse.aspx'})
+            req = session.post(url,data={'action':'savescoinfo160928','cid':cid,'scoid':id,'uid':uid,'progress':'100','crate':'100','status':'unknown','cstatus':'completed','trycount':'0'},headers={'Referer':'https://welearn.sflep.com/Student/StudyCourse.aspx?cid={}&classid={}&sco={}'.format(cid, classid, id)})
             if('"ret":0' in req.text):
                 print('Way2:Success!!!')
+                way2su += 1
             else:
                 print('Way2:Fail!!!')
+                way2fa += 1
         else:
             print(x['location'] + '   ' + x['iscomplete'])
     i+=1
     url = 'https://welearn.sflep.com/ajax/StudyStat.aspx?action=scoLeaves&cid=' + cid + '&uid=' + uid + '&unitidx=' + str(i) + '&classid=' + classid
     req = session.get(url,headers={'Referer':'https://welearn.sflep.com/2019/student/course_info.aspx?cid=' + cid})
-print('Finish!!\n\n\n**********  Created By Avenshy  **********')
+print('Finish!!\n')
+print('Total counts:\n')
+print('way1: {} succeeded, {} failed'.format(way1su, way1fa))
+print('way2: {} succeeded, {} failed'.format(way2su, way2fa))
+print("\n\n\n**********  Created By Avenshy  **********\n\n\n")
+input("Press any key to exit...")
