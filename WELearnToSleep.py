904b237ad2d6e0679bde14088e07866df340a09d
Avenshy <30952834+Avenshy@users.noreply.github.com>
2020-06-18 01:19:33 +0800

    Add files via upload

--- WELearnToSleep.py
+++ WELearnToSleep.py
@@ -1,41 +1,68 @@
 import requests
 import re
-import sys
+from sys import argv
 from time import sleep
-
+from random import randint
+session = requests.Session()
 print("**********  Created By Avenshy & SSmJaE  **********")
-print("                 Version:0.3dev")
+print("                 Version:0.4dev\n")
+print("     https://github.com/Avenshy/WELearnToSleep")
+print("   本软件遵守GPLv3协议，且免费、开源，禁止售卖!")
+print("   本软件遵守GPLv3协议，且免费、开源，禁止售卖!")
+print("   本软件遵守GPLv3协议，且免费、开源，禁止售卖!")
 print("***************************************************\n")
+
+def printline():
+    print('-'*51)
 
 # 获取账户密码
 try:  # 直接从命令行中获取
     username, password = sys.argv[1], sys.argv[2]
 except:
-    username = input('Username: ')
-    password = input('Password: ')
-print("Login...")
-
-# 登录模块
-session = requests.Session()
-loginUrl = "https://sso.sflep.com/cas/login?service=http%3a%2f%2fwelearn.sflep.com%2f2019%2fuser%2floginredirect.aspx"
-response = session.get(loginUrl)
-lt = re.search('name="lt" value="(.*?)"', response.text).group(1)
-response = session.post(loginUrl, data={"username": username,
-                                        "password": password,
-                                        "lt": lt,
-                                        "_eventId": "submit",
-                                        "submit": "LOGIN"})
-if "请登录" in response.text:
-    print("Login Failed!!")
-    exit(0)
-else:
-    print("Login Success!!", end="\n\n")
-
+    loginmode=input('请选择登录方式: \n  1.账号密码登录\n  2.Cookie登录\n\n请输入数字1或2: ')
+    printline()
+    if loginmode=='1':
+        username = input('请输入账号: ')
+        password = input('请输入密码: ')
+        # 登录模块
+        print('登录中...')
+        loginUrl = "https://sso.sflep.com/cas/login?service=http%3a%2f%2fwelearn.sflep.com%2f2019%2fuser%2floginredirect.aspx"
+        response = session.get(loginUrl)
+        lt = re.search('name="lt" value="(.*?)"', response.text).group(1)
+        response = session.post(loginUrl, data={"username": username,
+                                                "password": password,
+                                                "lt": lt,
+                                                "_eventId": "submit",
+                                                "submit": "LOGIN"})
+        if "请登录" in response.text:
+            input("登录失败!!")
+            exit(0)
+        else:
+            print("登录成功!!")
+    elif loginmode=='2':
+        try:
+            cookie = dict(map(lambda x:x.split('=',1),input('请粘贴Cookie: ').split(";")))
+        except:
+            input('Cookie输入错误!!!')
+            exit(0)
+        for k,v in cookie.items():
+              session.cookies[k]=v
+    else:
+        input('输入错误!!')
+        exit(0) 
+printline()
 while True:
     # 查询课程信息
     url = "https://welearn.sflep.com/ajax/authCourse.aspx?action=gmc"
     response = session.get(
         url, headers={"Referer": "https://welearn.sflep.com/2019/student/index.aspx"})
+    if '\"clist\":[]}' in response.text:
+        input('发生错误!!!可能是登录错误或没有课程!!!')
+        exit(0)
+    else:
+        print('查询课程成功!!!')
+        printline()
+        print('我的课程: \n')
     back = response.json()["clist"]
     for i, course in enumerate(back, start=1):
         print(f'[NO.{i:>2}] 完成度{course["per"]:>3}% {course["name"]}')
@@ -43,8 +70,9 @@ while True:
     # 选择课程
     order = int(input("\n请输入需要完成的课程序号（上方[]内的数字）: "))
     cid = back[order - 1]["cid"]
-    print("Running...")
-
+    printline()
+    print("获取单元中...")
+    printline()
     # 刷课模块
     url = f"https://welearn.sflep.com/2019/student/course_info.aspx?cid={cid}"
     response = session.get(url)
@@ -56,20 +84,26 @@ while True:
     response = session.get(url,params={'action':'courseunits','cid':cid,'uid':uid},headers={'Referer':'https://welearn.sflep.com/2019/student/course_info.aspx'})
     back = response.json()['info']
 
-    # 选择单元 直接复制了WELearnToSleeep的代码
-    print('\n\n[NO. 0]  按顺序完成全部单元课程')
-    i = 0
+    # 选择单元 使用了WELearnToSleeep的代码
+    print('[NO. 0]  按顺序完成全部单元课程')
     unitsnum = len(back)
-    for x in back:
-        i+=1
-        print('[NO.{:>2d}]  {}  {}'.format(i,x['unitname'],x['name']))
+    for i,x in enumerate(back,start=1):
+        if x['visible']=='true':
+            print(f'[NO.{i:>2d}]  [已开放]  {x["unitname"]}  {x["name"]}')
+        else:
+            print(f'[NO.{i:>2d}] ![未开放]! {x["unitname"]}  {x["name"]}')
     unitidx = int(input('\n\n请选择需要完成的单元序号（上方[]内的数字，输入0为按顺序刷全部单元）： '))
-
-
-
+    printline()
+    inputcrate = input('模式1:每个练习指定正确率，请直接输入指定的正确率\n如:希望每个练习正确率均为100，则输入 100\n\n模式2:每个练习随机正确率，请输入正确率上下限并用英文逗号隔开\n如:希望每个练习正确率为70～100，则输入 70,100\n\n请严格按照以上格式输入每个练习的正确率: ')
+    if ',' in inputcrate:
+        mycrate=eval(inputcrate)
+        randommode=True
+    else:
+        mycrate=inputcrate
+        randommode=False
+    printline()
     # 伪造请求
     way1Succeed, way2Succeed, way1Failed, way2Failed = 0, 0, 0, 0
-    data = '{"cmi":{"completion_status":"completed","interactions":[],"launch_data":"","progress_measure":"1","score":{"scaled":"100","raw":"100"},"session_time":"0","success_status":"unknown","total_time":"0","mode":"normal"},"adl":{"data":[]},"cci":{"data":[],"service":{"dictionary":{"headword":"","short_cuts":""},"new_words":[],"notes":[],"writing_marking":[],"record":{"files":[]},"play":{"offline_media_id":"9999"}},"retry_count":"0","submit_time":""}}[INTERACTIONINFO]'
 
     ajaxUrl = "https://welearn.sflep.com/Ajax/SCO.aspx"
     infoHeaders = {
@@ -90,14 +124,21 @@ while True:
             break
 
         for course in response.json()["info"]:
-            if "未" in course["iscomplete"]:  # 章节未完成
-                print(f'[未完成]    {course["location"]}')
+            if course['isvisible']=='false':  # 跳过未开放课程
+                print(f'[!!跳过!!]    {course["location"]}')
+            elif "未" in course["iscomplete"]:  # 章节未完成
+                print(f'[即将完成]    {course["location"]}')
+                if randommode is True:
+                    crate=str(randint(mycrate[0],mycrate[1]))
+                else:
+                    crate=mycrate
+                data = '{"cmi":{"completion_status":"completed","interactions":[],"launch_data":"","progress_measure":"1","score":{"scaled":"'+crate+'","raw":"100"},"session_time":"0","success_status":"unknown","total_time":"0","mode":"normal"},"adl":{"data":[]},"cci":{"data":[],"service":{"dictionary":{"headword":"","short_cuts":""},"new_words":[],"notes":[],"writing_marking":[],"record":{"files":[]},"play":{"offline_media_id":"9999"}},"retry_count":"0","submit_time":""}}[INTERACTIONINFO]'
+
                 id = course["id"]
                 session.post(ajaxUrl, data={"action": "startsco160928",
                                             "cid": cid,
                                             "scoid": id,
-                                            "uid": uid,
-                                            "nocache": "0.0429450926459094",
+                                            "uid": uid
                                             },
                              headers={"Referer": f"https://welearn.sflep.com/Student/StudyCourse.aspx?cid={cid}&classid={classid}&sco={id}"})
                 response = session.post(ajaxUrl, data={"action": "setscoinfo",
@@ -105,13 +146,14 @@ while True:
                                                        "scoid": id,
                                                        "uid": uid,
                                                        "data": data,
-                                                       "isend": "False", },
+                                                       "isend": "False" },
                                         headers={"Referer": f"https://welearn.sflep.com/Student/StudyCourse.aspx?cid={cid}&classid={classid}&sco={id}"})
+                print(f'>>>>>>>>>>>>>>正确率:{crate:>3}%',end='  ')
                 if '"ret":0' in response.text:
-                    print("[已模拟]    Way1:Success!!!", end="  ")
+                    print("方式1:成功!!!", end="  ")
                     way1Succeed += 1
                 else:
-                    print("[已模拟]    Way1:Failed !!!", end="  ")
+                    print("方式1:失败!!!", end="  ")
                     way1Failed += 1
 
                 response = session.post(ajaxUrl, data={"action": "savescoinfo160928",
@@ -119,21 +161,21 @@ while True:
                                                        "scoid": id,
                                                        "uid": uid,
                                                        "progress": "100",
-                                                       "crate": "100",
+                                                       "crate": crate,
                                                        "status": "unknown",
                                                        "cstatus": "completed",
                                                        "trycount": "0",
                                                        },
                                         headers={"Referer": f"https://welearn.sflep.com/Student/StudyCourse.aspx?cid={cid}&classid={classid}&sco={id}"})
-                sleep(1) # 延迟1秒防止服务器压力过大
+#                sleep(1) # 延迟1秒防止服务器压力过大
                 if '"ret":0' in response.text:
-                    print("Way2:Success!!!")
+                    print("方式2:成功!!!")
                     way2Succeed += 1
                 else:
-                    print("Way2:Fail   !!!")
+                    print("方式2:失败!!!")
                     way2Failed += 1
             else:  # 章节已完成
-                print(f'[已完成]    {course["location"]}')
+                print(f'[ 已完成 ]    {course["location"]}')
 
         if unitidx != 0:
             break
@@ -143,13 +185,20 @@ while True:
         break
     else:
         print('本单元运行完毕！回到选课处！！\n\n\n\n')
+        printline()
 
-
+printline()
 print(f"""
 ***************************************************
-Finish!!
-Total counts:
-way1: {way1Succeed} succeeded, {way1Failed} failed
-way2: {way2Succeed} succeeded, {way2Failed} failed
+全部完成!!
+
+总计:
+方式1: {way1Succeed} 成功, {way1Failed} 失败
+方式2: {way2Succeed} 成功, {way2Failed} 失败
+
+https://github.com/Avenshy/WELearnToSleep
+本软件遵守GPLv3协议，且免费、开源，禁止售卖!
+本软件遵守GPLv3协议，且免费、开源，禁止售卖!
+本软件遵守GPLv3协议，且免费、开源，禁止售卖!
 **********  Created By Avenshy & SSmJaE  **********""")
 input("Press any key to exit...")
